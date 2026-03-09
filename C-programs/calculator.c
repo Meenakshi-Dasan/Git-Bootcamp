58970dc35bdc54968f463fe73ce3a75935b0a1b8
Meenakshi-Dasan <meenakshidasan149@gmail.com>
2026-03-08 23:16:00 -0800

    Add addition operation to calculator

---
+++ C-programs/calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double p,q;
+    do
+    {
+        char r;
+        printf("\nEnter choice:");
+        scanf("%c",&r);
+        switch(op)
+        {
+            case'a':
+              printf("Enter a number:");
+              scanf("%lf",&p);
+              printf("Enter another number:");
+              scanf("%lf",&q);
+              printf("\n%lf",(p+q));
+              break;
+            case'b':
+              printf("Enter a number:");
+              scanf("%lf",&p);
+              printf("Enter another number:");
+              scanf("%lf",&q);
+              printf("\n%lf",(p-q));
+              break;
+            case'c':
+              printf("Enter a number:");
+              scanf("%lf",&p);
+              printf("Enter another number:");
+              scanf("%lf",&q);
+              printf("\n%lf",(p*q));
+              break;
+             case'd':
+              printf("Enter a number:");
+              scanf("%lf",&p);
+              printf("Enter another number:");
+              scanf("%lf",&q);
+              printf("\n%lf",(p/q));
+              break;
+            case'e':
+              printf("Enter a number:");
+              scanf("%lf",&p);
+              printf("Enter another number:");
+              scanf("%lf",&q);
+              printf("\n%lf",pow(p,q));
+              break;
+            default:
+              printf("\n Exit");
+              break;
+        }
+    }
+    while(p!=0);
+return o;
+}
