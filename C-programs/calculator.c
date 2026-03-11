06d7e77254e86bd91a6de31156d2c759faa6521b
Meenakshi-Dasan <meenakshidasan149@gmail.com>
2026-03-10 19:07:05 -0800

    Added multiplication operation

--- C-programs/calculator.c
+++ C-programs/calculator.c
@@ -23,6 +23,13 @@ int main()
     scanf("%f",&y);
     printf("\n%0.2f",(x-y));
     break;
+    case 'c':
+    printf("Enter first number:");
+    scanf("%f",&x);
+    printf("Enter second number:");
+    scanf("%f",&y);
+    printf("\n%0.2f",(x*y));
+    break;
     default:
         printf("Exit");
     break;
