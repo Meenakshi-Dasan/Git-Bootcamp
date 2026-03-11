9cc146e746ae5815110cd811a15847be17b950cc
Meenakshi-Dasan <meenakshidasan149@gmail.com>
2026-03-10 19:07:53 -0800

    Added division operation

--- C-programs/calculator.c
+++ C-programs/calculator.c
@@ -30,6 +30,13 @@ int main()
     scanf("%f",&y);
     printf("\n%0.2f",(x*y));
     break;
+    case 'd':
+    printf("Enter first number:");
+    scanf("%f",&x);
+    printf("Enter second number:");
+    scanf("%f",&y);
+    printf("\n%0.2f",(x/y));
+    break;
     default:
         printf("Exit");
     break;
