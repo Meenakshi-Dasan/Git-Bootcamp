5a797c4b627586c778b953ef71375ccce14347d7
Meenakshi-Dasan <meenakshidasan149@gmail.com>
2026-03-13 18:35:15 -0800

    Entered a date

--- C-programs/date.c
+++ C-programs/date.c
@@ -1,40 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int d,m,y;
+    int d,m y;
     printf("Enter the date(d,m,y):");
-    scanf("%d/%d/%d",&d,&m,&y);
-    if(d==1||d==21||d==31)
-    printf("%dst day of",d);
-    else if("d==2||d==22")
-    printf("%dnd day of",d);
-    else if("d==3||d==23")
-    printf("%drd day of",d);
-    else
-    printf("%dth day of",d);
-    switch(m)
-    {
-        case 1:
-        printf("January");
-        break;
-        case 2:
-        printf("February");
-        break;
-        case 3:
-        printf("March");
-        break;
-        case 4:
-        printf("April");
-        break;
-        case 5:
-        printf("May");
-        break;
-        case 6:
-        printf("January");
-        break;
-        case 7:
-        printf("January");
-        break;
-
-    }
+    scanf("%d/%d/%d",&d,%m,%y);
+    
 }
